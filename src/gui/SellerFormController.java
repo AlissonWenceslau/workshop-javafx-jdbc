63dd0c015b6918bc81d5f9b1a52f14e3037730b3
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-01-03 21:54:48 -0300

    Saving Seller

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,9 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -115,6 +117,7 @@ public class SellerFormController implements Initializable {
 		Utils.currentStage(event).close();
 	}
 
+	//Pega os dados preenchidos no formulário e carrega em um objeto
 	private Seller getFormData() {
 		Seller obj = new Seller();
 
@@ -126,11 +129,33 @@ public class SellerFormController implements Initializable {
 			exception.addError("name", "Field can't be empty");
 		}
 		obj.setName(txtName.getText());
+		
+		if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+			exception.addError("email", "Field can't be empty");
+		}
+		obj.setEmail(txtEmail.getText());
+		
+		/*Pegar valor do datePicker
+		//Converte a data escolhida no computador do usuário para instant, que é uma data independente da localidade
+		 */
+		if(dpBirthDate.getValue() == null) {
+			exception.addError("birthDate", "Field can't be empty");
+		}else {
+			Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+			obj.setBirthDate(Date.from(instant));
+		}
+		
+		if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")) {
+			exception.addError("baseSalary", "Field can't be empty");
+		}
+		obj.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText()));
 
+		obj.setDepartment(comboBoxDepartment.getValue());
+		
 		if (exception.getErrors().size() > 0) {
 			throw exception;
 		}
-
+		
 		return obj;
 	}
 
@@ -170,8 +195,10 @@ public class SellerFormController implements Initializable {
 		
 		if(entity.getDepartment() == null) {
 			comboBoxDepartment.getSelectionModel().selectFirst();
+		}else {
+			comboBoxDepartment.setValue(entity.getDepartment());			
 		}
-		comboBoxDepartment.setValue(entity.getDepartment());
+
 	}
 
 	public void loadAssociatedObjects() {
@@ -184,12 +211,14 @@ public class SellerFormController implements Initializable {
 		comboBoxDepartment.setItems(obsList);
 	}
 
+	//Testa cada um dos possíveis erros e seta o valor do label correspondente
 	public void setErrorMessages(Map<String, String> errors) {
 		Set<String> fields = errors.keySet();
 
-		if (fields.contains("name")) {
-			lblErrorName.setText(errors.get("name"));
-		}
+		lblErrorName.setText(fields.contains("name") ? errors.get("name") : "");
+		lblErrorEmail.setText(fields.contains("email") ? errors.get("email") : "");
+		lblErrorBirthDate.setText(fields.contains("birthDate") ? errors.get("birthDate") : "");
+		lblErrorBaseSalary.setText(fields.contains("baseSalary") ? errors.get("baseSalary") : "");
 	}
 
 	private void initializeComboBoxDepartment() {
